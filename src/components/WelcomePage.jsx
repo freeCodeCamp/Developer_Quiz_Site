c6fb507d4e3b3b7227a1dfc1946303c0c13fdb81
Dionysia <dionylemonakh@gmail.com>
2021-12-15 19:24:12 +0200

    Made some small changes to the text on the welcome page

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,13 +1,14 @@
 const WelcomePage = ({ start }) => {
   return (
     <>
-      <h1>Welcome!</h1>
-      <h2>Want to test your knowledge?</h2>
-      <p>Take our quiz and have fun learning</p>
+      <h1>Welcome to Learn to Code RPG Multiple Choice Questions!</h1>
+      <h2>Want to test your  programming knowledge?</h2>
+      <p>Brush up on HTML,CSS,JavaScript,Linux,Python,Git,SQL,IT and general Computer Science Concepts, with 600+ questions.</p>
+      <p>Take our quiz and have fun learning!</p>
       <button onClick={start}>Start Quiz</button>
       <h2>Brand new to programming?</h2>
-      <p>Learn to code with <a target="_blank" rel="noopener noreferrer" href="https://www.freecodecamp.org/learn/">freeCodeCamp</a></p>
-      <p>You can also play the freeCodeCamp <a href="/" target="_blank" rel="noopener noreferrer">RPG Game</a> </p>
+      <p>Learn to code for free and start your programming journey with <a target="_blank" rel="noopener noreferrer" href="https://www.freecodecamp.org/learn/">freeCodeCamp</a></p>
+      <p>Want to learn how to code while playing a game?  Download the freeCodeCamp <a href="/" target="_blank" rel="noopener noreferrer">Learn to Code RPG Game</a> </p>
     </>
   )
 }
