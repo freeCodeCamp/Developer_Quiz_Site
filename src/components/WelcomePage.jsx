c121eda6d92d6c78bf33feb3ed37c6433cf637bc
jdwilkin4 <jwilkin4@hotmail.com>
2021-12-16 12:43:31 -0800

    styling homepage

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,36 +1,44 @@
 import rpgMenu from '../images/rpg-menu.png';
+import homeImg1 from '../images/home-img1.jpg';
+import homeImg2 from '../images/home-img2.jpg';
 
 const WelcomePage = ({ start }) => {
   const imgWidth = { width: '80%' }
   return (
     <>
-      <header className="text-center">
-        <h1 className="mt-2">Learn to Code RPG Multiple Choice Questions!</h1>
-        <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img src={rpgMenu} style={imgWidth} alt="freeCodeCamp RPG game" />
-            </div>
-            <div className="carousel-item">
-              <img src="..." style={imgWidth} alt="freeCodeCamp RPG game" />
-            </div>
-            <div className="carousel-item">
-              <img src="..." style={imgWidth} alt="freeCodeCamp RPG game" />
-            </div>
+      <main className="text-center">
+        <h1 className="mt-2 featurette-heading">Learn to Code RPG Multiple Choice Questions!</h1>
+        <img src={rpgMenu} className="img-fluid" style={imgWidth} alt="freeCodeCamp" />
+
+        <hr className="featurette-divider" />
+
+        <div className="row featurette">
+          <div className="col-md-7">
+            <h2 className="featurette-heading">Want to test your  programming knowledge?</h2>
+            <p className="lead">Brush up on HTML, CSS, JavaScript, Linux, Python, Git, SQL, IT and general Computer Science concepts, with <span style={{ fontWeight: '700' }}>600+</span> questions.</p>
+            <p className="lead">Take our quiz and have fun learning!</p>
+            <button className="btn btn-primary btn-xl rounded-pill" onClick={start}>Start Quiz</button>
+          </div>
+          <div className="col-md-5">
+            <img src={homeImg1} className="img-fluid rounded" style={imgWidth} alt="female software engineers" />
           </div>
         </div>
 
-        <h2>Want to test your  programming knowledge?</h2>
-        <p>Brush up on HTML, CSS, JavaScript, Linux, Python, Git, SQL, IT and general Computer Science Concepts, with 600+ questions.</p>
-        <p>Take our quiz and have fun learning!</p>
-        <button className="btn btn-primary btn-xl rounded-pill" onClick={start}>Start Quiz</button>
-        <h2>Brand new to programming?</h2>
-        <p>Learn to code for free and start your programming journey with <a target="_blank" rel="noopener noreferrer" href="https://www.freecodecamp.org/learn/">freeCodeCamp</a>.</p>
-        <h3>Want to learn how to code while playing a game? </h3>
-        <p> Give the freeCodeCamp <a href="/" target="_blank" rel="noopener noreferrer">Learn to Code RPG Game</a> a go! </p>
-        <p>Available for free download  on Windows, Mac and Linux.</p>
-      </header>
+        <hr className="featurette-divider" />
 
+        <div className="row featurette">
+          <div className="col-md-7 order-md-2">
+            <h2 className="featurette-heading">Brand new to programming?</h2>
+            <p className="lead">Learn to code for free and start your programming journey with <a target="_blank" rel="noopener noreferrer" href="https://www.freecodecamp.org/learn/">freeCodeCamp</a>.</p>
+            <h3 style={{ marginTop: '40px' }} className="featurette-heading">Want to learn how to code while playing a game?</h3>
+            <p className="lead"> Give the freeCodeCamp <a href="/" target="_blank" rel="noopener noreferrer">Learn to Code RPG Game</a> a go!</p>
+            <p className="lead">Available for free download on Windows <i style={{ color: '#00A4EF' }} className="fab fa-windows" />, Mac <i className="fab fa-apple" /> and Linux <i className="fab fa-linux" />.</p>
+          </div>
+          <div className="col-md-5 order-md-1">
+            <img src={homeImg2} className="img-fluid rounded" style={imgWidth} alt="female software engineers" />
+          </div>
+        </div>
+      </main>
     </>
   )
 }
