54e4588ffdee3786c400ea66a7b7d297cf0f4663
estefaniacn <85124911+estefaniacn@users.noreply.github.com>
2021-12-19 20:43:28 -0400

    fix: updating Button component with conditional rendering

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,29 +2,16 @@ import { Link } from 'react-router-dom';
 import './Button.css';
 
 const Button = ({text, path, isTransparent}) => {
-  if (!isTransparent) {
-    return (
-      <>
-        <Link to={path}>
-          <button 
-            className='btn-default'>
-            {text}
-          </button>
-        </Link>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Link to={path}>
-          <button 
-            className='btn-default transparent-btn'>
-            {text}
-          </button>
-        </Link>
-      </>
-    );
-  }
+  return (
+    <>
+      <Link to={path}>
+        <button
+          className={!isTransparent ? 'btn-default' : 'btn-default transparent-btn'}>
+          {text}
+        </button>
+      </Link>
+    </>
+  );
 }
 
 export default Button;
