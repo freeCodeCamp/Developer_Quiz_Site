a2c316d29fe8123d2e07a6d4818ab1b6f6584430
jdwilkin4 <jwilkin4@hotmail.com>
2021-12-19 22:53:17 -0800

    feat: adding ScrollToTop component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import WelcomePage from './components/WelcomePage';
 import QuizTemplate from './components/QuizTemplate';
 import { Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 
 const App = () => {
   return (
     <>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/quizPage" element={<QuizTemplate />} />
