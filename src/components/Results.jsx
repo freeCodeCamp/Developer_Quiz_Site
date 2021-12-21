5454d8a018500496e78d59df668d4e748acbd1f8
jdwilkin4 <jwilkin4@hotmail.com>
2021-12-20 19:48:24 -0800

    style: styling results rpg link

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,7 +3,14 @@ const Results = ({ points, totalPoints, resetQuiz }) => {
     <div className="results-div" >
       <h1 className="results-heading">Results</h1>
       <h2>Total user points: {points} out of {totalPoints}</h2>
-      <p className="results-text">Wanna learn how to code? Download the free <a target="_blank" rel="noopener noreferrer" href="https://www.freecodecamp.org/news/learn-to-code-rpg/">RPG game</a></p>
+      <p className="results-text">Wanna learn how to code? Download the free
+        <a
+          className="results-rpg-link"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.freecodecamp.org/news/learn-to-code-rpg/"> RPG game
+        </a>
+      </p>
       <button
         onClick={resetQuiz}
         className="results-btn"
