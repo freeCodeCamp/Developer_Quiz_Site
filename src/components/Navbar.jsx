a2173fc5e232bdaf879af8bbaadae7a2f51ed84d
jdwilkin4 <jwilkin4@hotmail.com>
2021-12-22 07:35:59 -0800

    fix: fixing path for navbar image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import '../stylesheets/Navbar.css';
+import fccLogo from '../images/fcc_primary_large.png'
 
 const Navbar = () => {
   return (
     <>
       <header>
         <nav className="homepage-navbar">
-          <img className="website-logo" src="../../fcc_primary_large.png" alt="freeCodeCamp logo" />
+          <img className="website-logo" src={fccLogo} alt="freeCodeCamp logo" />
         </nav>
       </header>
     </>
