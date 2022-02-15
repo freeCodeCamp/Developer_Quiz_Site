e95b97a9e1f239b13b839b642da8c1c98600ec4b
Tawhid Monowar <tawhidmonowar@gmail.com>
2022-02-15 14:43:15 +0600

    Remove extra space in the "RPG game" link.
    In the result view, there is a space before the "RPG game" link. It's not looking good, because the underline is shown in space, and it looks like "_RPG game". So I removed a space from the start of <a> tag and added a space at end of <p> tag, and It looks fine :)

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,12 +3,12 @@ const Results = ({ points, totalPoints, resetQuiz }) => {
     <div className="results-div" >
       <h1 className="results-heading">Results</h1>
       <h2>{points === totalPoints ? 'Wow! Perfect Score!' : 'You received'} {points} out of {totalPoints} points</h2>
-      <p className="results-text">Wanna learn how to code? Download the free
+      <p className="results-text">Wanna learn how to code? Download the free 
         <a
           className="results-rpg-link"
           target="_blank"
           rel="noopener noreferrer"
-          href="https://www.freecodecamp.org/news/learn-to-code-rpg/"> RPG game
+          href="https://www.freecodecamp.org/news/learn-to-code-rpg/">RPG game
         </a>
       </p>
       <button
