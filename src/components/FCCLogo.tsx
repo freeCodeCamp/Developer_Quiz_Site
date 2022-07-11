0de97cb6311e672ced1476ad027b53155ff18e8b
Aaron <a.rcottrill521@gmail.com>
2022-07-11 03:26:42 -0400

    Convert all images to the webp format. (#84)

--- src/components/FCCLogo.tsx
+++ src/components/FCCLogo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import fCClogo from "../images/fcc_primary_large.png";
+import fCClogo from "../images/fcc_primary_large.webp";
 
 const FCCLogo: React.FC = () => {
   return <img className="fcc-logo" src={fCClogo} alt="freeCodeCamp logo" />;
