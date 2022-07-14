16f4e1f8c88429541160c9e4792ab32e8ab424eb
Aaron <a.rcottrill521@gmail.com>
2022-07-14 19:49:36 -0400

    Chore: Create tests for a few components
    I wanted there to be a some tests set up because every single
    app ideally should have more than the default tests set up.
    As some components are intertwined , I am saving those for a
    later date. I am interested in hearing further clarification
    regarding the SelectQuiz component and if it would be wise
    to edit it such that the last index is always the largest
    amount of questions. Jest and React Testing Library are
    currently powering the tests, further tools can be added
    as required.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import SelectQuiz from "./components/SelectQuiz";
 import WelcomePage from "./components/WelcomePage";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import Results from "./components/Results";
+
+afterEach(cleanup);
 
 describe("App", () => {
   it("renders without crashing", () => {
@@ -14,5 +19,85 @@ describe("Welcome Page", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<WelcomePage start={undefined} />, div);
+  });
+});
+
+describe("Select Quiz", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(
+      <SelectQuiz
+        startQuiz={undefined}
+        selectQuizArr={[10, 25, 50, 100, 681]}
+      />,
+      div
+    );
+  });
+  it("Has a button for every quiz question amount under 600", () => {
+    const { getByText } = render(
+      <SelectQuiz
+        startQuiz={undefined}
+        selectQuizArr={[10, 25, 50, 100, 599]}
+      />
+    );
+    expect(getByText("10").textContent).toBeDefined();
+    expect(getByText("25").textContent).toBeDefined();
+    expect(getByText("50").textContent).toBeDefined();
+    expect(getByText("100").textContent).toBeDefined();
+    // TODO: Ensure it isn't supposed to be All (599) here
+    expect(getByText("599").textContent).toBeDefined();
+  });
+  it("Has a button for every quiz question amount for max amount equal to 600", () => {
+    const { getByText } = render(
+      <SelectQuiz
+        startQuiz={undefined}
+        selectQuizArr={[10, 25, 50, 100, 600]}
+      />
+    );
+    expect(getByText("10").textContent).toBeDefined();
+    expect(getByText("25").textContent).toBeDefined();
+    expect(getByText("50").textContent).toBeDefined();
+    expect(getByText("100").textContent).toBeDefined();
+    // TODO: Ensure it isn't supposed to be All (600) here
+    expect(getByText("600").textContent).toBeDefined();
+  });
+  it("Has a button for every quiz question for max amount equal to 601", () => {
+    const { getByText } = render(
+      <SelectQuiz
+        startQuiz={undefined}
+        selectQuizArr={[10, 25, 50, 100, 601]}
+      />
+    );
+    expect(getByText("10").textContent).toBeDefined();
+    expect(getByText("25").textContent).toBeDefined();
+    expect(getByText("50").textContent).toBeDefined();
+    expect(getByText("100").textContent).toBeDefined();
+    expect(getByText("All (601)").textContent).toBeDefined();
+  });
+});
+
+describe("Results", () => {
+  it("Renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(
+      <Results points={0} totalPoints={10} resetQuiz={undefined} />,
+      div
+    );
+  });
+  it("Displays score accurately if wrong answers", () => {
+    const { getByText } = render(
+      <Results points={0} totalPoints={10} resetQuiz={undefined} />
+    );
+    expect(
+      getByText("You received 0 out of 10 points").textContent
+    ).toBeDefined();
+  });
+  it("Displays score accurately if all answers correct", () => {
+    const { getByText } = render(
+      <Results points={10} totalPoints={10} resetQuiz={undefined} />
+    );
+    expect(
+      getByText("Wow! Perfect Score! 10 out of 10 points").textContent
+    ).toBeDefined();
   });
 });
