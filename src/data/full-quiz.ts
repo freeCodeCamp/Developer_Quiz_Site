023d408295199d7709ca85b4e44f63ed981cb7c5
Daniele <daniele.registrazioni@outlook.it>
2022-09-18 22:21:43 +0200

    feat: Add four Agile questions (#108)
    * feat: Add four Agile questions
    
    * Apply suggestions from code review
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>
    
    * Syntax-fix on full-quiz.ts
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/full-quiz.ts
+++ src/data/full-quiz.ts
@@ -17,7 +17,7 @@ const fullQuiz = [
     Distractor1: ".substring()",
     Distractor2: ".reduce()",
     Distractor3: ".slice()",
-    Explanation: 
+    Explanation:
       "The .trim() method removes white space (including space, tab, etc.) from both ends of a string and returns a new string without modifying the original.",
     Link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trim",
   },
@@ -7195,6 +7195,50 @@ const fullQuiz = [
     Explanation:
       "Waterfall is an opposing project management methodology that has little to no room for feedback or iteration until the product is completely built and tested.",
     Link: "https://www.freecodecamp.org/news/agile-methods-and-methodology-for-beginners/",
+  },
+  {
+    Question:
+      "In a Scrum team, what is a Scrum Master?",
+    Answer: "A servant leader whose role is to support the rest of the team and the organization",
+    Distractor1: "A project manager who's main role is to ensure the Developers deliver in time",
+    Distractor2: "An external resource temporarily assigned to the team to audit their work",
+    Distractor3: "Scrum Master is not a role officially recognized by the Scrum guide",
+    Explanation:
+      "The Scrum Master is a leader who supports their colleagues in their activities and does more than just assign tasks.",
+    Link: "https://www.freecodecamp.org/news/what-is-a-scrum-master-the-agile-role-and-responsibilities-explained/",
+  },
+  {
+    Question:
+      "In a Scrum team, who is responsible for stories estimation?",
+    Answer: "The Developers",
+    Distractor1: "The Product Owner",
+    Distractor2: "The Scrum Master",
+    Distractor3: "The Product Owner and the Scrum Master",
+    Explanation:
+      "According to the Scrum Guide, only members of the development team are allowed to estimate development effort. Neither Scrum Master, nor Product Owner.",
+    Link: "https://www.freecodecamp.org/news/scrum-the-hard-parts/",
+  },
+  {
+    Question:
+      "In a Scrum team, who is responsible for setting priorities?",
+    Answer: "The Product Owner",
+    Distractor1: "The Developers",
+    Distractor2: "The Scrum Master",
+    Distractor3: "The Product Owner and the Scrum Master",
+    Explanation:
+      "According to the Scrum Guide, the Product Owner has the final say when prioritizing the Product Backlog.",
+    Link: "https://www.freecodecamp.org/news/scrum-the-hard-parts/",
+  },
+  {
+    Question:
+      "In a project adopting the Scrum framework, when is the Product Backlog completed?",
+    Answer: "Never",
+    Distractor1: "Before the project starts",
+    Distractor2: "There is no precise limit, but it is advised to complete it before the fourth Sprint",
+    Distractor3: "It is mandatory for the Product Backlog to be completed before the fourth Sprint",
+    Explanation:
+      "According to the Scrum Guide, the Product Backlog is never complete. Initially, it contains only the best-understood requirements.",
+    Link: "https://www.freecodecamp.org/news/scrum-the-hard-parts-2-sprint-harder/",
   },
 ];
 export default fullQuiz;
