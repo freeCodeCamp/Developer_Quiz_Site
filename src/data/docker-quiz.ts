07cca25e0fd2f34aef2d246cd63513009b3ea939
Jeremy King <jking@beckermedia.net>
2022-09-23 16:56:05 -0600

    update docker question

--- src/data/docker-quiz.ts
+++ src/data/docker-quiz.ts
@@ -1,13 +1,13 @@
 const dockerQuiz = [ 
     {
         Question: "What forum tool does freeCodeCamp use for its forum?",
-        Answer: "Discourse",
-        Distractor1: "NodeBB",
-        Distractor2: "phpBB",
-        Distractor3: "vBulletin",
+        Answer: "An open source platform that enables developers to build, deploy, run, update and manage containers",
+        Distractor1: "An open source platform that enables developers to build, deploy, run, update and manage Operating systems",
+        Distractor2: "An open source platform that enables developers to build, deploy, run, update and manage Web sites",
+        Distractor3: "An open source platform that enables developers to build, deploy, run, update and manage Servers",
         Explanation:
-          "The freeCodeCamp community was an early adopter of Discourse, a powerful forum tool designed by Stack Overflow founder Jeff Atwood. Quincy Larson first met Jeff at an event in San Francisco in 2014, and the two talked about online communities. Jeff convinced Quincy to create a forum so that learners could easily help one another. One benefit of a forum is that other people can then discover past conversations, and use them to help get unstuck. If you ask a question on the freeCodeCamp forum, you will generally get an answer in just a few hours.",
-        Link: "https://www.freecodecamp.org/news/the-future-of-the-freecodecamp-forum/",
+          "Docker is an open source platform that enables developers to build, deploy, run, update and manage containers—standardized, executable components that combine application source code with the operating system (OS) libraries and dependencies required to run that code in any environment.",
+        Link: "https://www.ibm.com/cloud/learn/docker",
       },
 ];
 
