a23301f87d387764e5aef04f87baed470b1227c4
Jeremy King <jking@beckermedia.net>
2022-09-27 16:09:00 -0600

    add missing files

---
+++ src/data/information-technology-quiz.ts
@@ -0,0 +1,59 @@
+const informationTechnologyQuiz = [
+  {
+    Question: "What is PRAM?",
+    Answer:
+      "A battery powered RAM for older Mac computers that holds important information for your system",
+    Distractor1: "A newer type of RAM for Mac computers",
+    Distractor2: "A newer type of RAM for Windows Computers",
+    Distractor3:
+      "A battery powered RAM for older PC computers that holds important information for your system",
+    Explanation:
+      "Parameter Random Access Memory is a battery powered RAM for older Mac computers that holds important information for your system.",
+    Link: "https://www.freecodecamp.org/news/pram-definition/",
+  },
+  {
+    Question: "What does LCD stand for?",
+    Answer: "Liquid Crystal Display",
+    Distractor1: "Label Crystal Display",
+    Distractor2: "Liquid Cache Display",
+    Distractor3: "Liquid Crystal Dictionary",
+    Explanation:
+      "Liquid Crystal Display is a type of display that uses liquid crystals to display images.",
+    Link: "https://www.freecodecamp.org/news/lcd-definition/",
+  },
+  {
+    Question:
+      "Which hardware component has all of its data wiped when the power turns off? ",
+    Answer: "RAM (Random Access Memory)",
+    Distractor1: "HDD (Hard Disk Drive)",
+    Distractor2: "SSD (Solid State Drive)",
+    Distractor3: "CPU (Central Processing Unit)",
+    Explanation:
+      "RAM (short for Random Access Memory), or the main memory, is a volatile, short-term type of memory that only stores information temporarily while a computer is powered by electricity. It is used when you open and are using an application or file. When the power turns off, any files you created or updates you made and didn't save, will be lost and will be hard to retrieve.",
+    Link: "https://www.freecodecamp.org/news/what-is-a-pc-computer-definition-and-computer-basics-for-beginners/",
+  },
+  {
+    Question:
+      "Which part of the CPU is responsible for carrying out mathematical operations?",
+    Answer: "The Arithmetic Logic Unit",
+    Distractor1: "The Control Unit",
+    Distractor2: "Registers",
+    Distractor3: "Cache Memory",
+    Explanation:
+      "The ALU (short for Arithmetic Logic Unit), is a digital circuit that performs arithmetic operations such as addition, subtraction, multiplication, and division.",
+    Link: "https://www.freecodecamp.org/news/what-is-cpu-meaning-definition-and-what-cpu-stands-for/",
+  },
+  {
+    Question: "What is a Solid State Drive?",
+    Answer:
+      "A type of storage device that uses flash memory to store and access data",
+    Distractor1: "A device to record text to a disk",
+    Distractor2: "A device to translate code from one language to another ",
+    Distractor3: "A screen editor used for Unix",
+    Explanation:
+      "A type of storage device that uses flash memory to store and access data",
+    Link: "https://www.freecodecamp.org/news/ssd-solid-state-drive-definition/",
+  },
+];
+
+export default informationTechnologyQuiz;
---
+++ src/data/security-quiz.ts
@@ -0,0 +1,26 @@
+const securityQuiz = [
+{
+    Question:
+      "Which of the following type of Hackers look for vulnerable computers over the internet, exploit them and use them for their advantage?",
+    Answer: "Black Hat Hackers",
+    Distractor1: "Red Hat Hackers",
+    Distractor2: "White Hat Hackers",
+    Distractor3: "Grey Hat Hackers",
+    Explanation:
+      "Black Hat Hackers exploit vulnerable computers over the internet by installing backdoors, malwares, sending phishing emails and links , therby getting access to private and sensitive data",
+    Link: "https://www.freecodecamp.org/news/white-hat-black-hat-red-hat-hackers/",
+  },
+  {
+    Question:
+      "Which of the following type of Hackers protect civilians and organizations by finding and reporting vulnerabilities in the system?",
+    Answer: "White Hat Hackers",
+    Distractor1: "Grey Hat Hackers",
+    Distractor2: "Black Hat Hackers",
+    Distractor3: "Red Hat Hackers",
+    Explanation:
+      "White Hat Hackers work for organizations as Cybersecurity Engineers, Penetration Testers, etc.  where they perform tasks like ethically hacking the system to find and report vulnerabilities. ",
+    Link: "https://www.freecodecamp.org/news/white-hat-black-hat-red-hat-hackers/",
+  },
+];
+
+export default securityQuiz;
