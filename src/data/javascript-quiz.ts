1fd4a053602b48a9e26e610a02509807781ddfc9
Preethi⚡ <69065327+Preethi-Dev@users.noreply.github.com>
2022-10-01 21:59:00 +0530

    chore: Added a new JavaScript quiz question (#127)
    * Add a js quiz question
    
    * Update src/data/javascript-quiz.ts
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/javascript-quiz.ts
+++ src/data/javascript-quiz.ts
@@ -11,6 +11,17 @@ const javascriptQuiz = [
   },
   {
     Question:
+      "Which Web API used with JavaScript provides information about the size and position of an element?",
+    Answer: ".getBoundingClientRect()",
+    Distractor1: ".getOwnPropertyDescriptors()",
+    Distractor2: ".assign()",
+    Distractor3: ".entries()",
+    Explanation:
+      "The Element.getBoundingClientRect() method returns a DOMRect object providing information about the size of an element and its position relative to the viewport.",
+    Link: "https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect",
+  },
+  {
+    Question:
       "In JavaScript, what is the name of the method used to remove white space from the beginning and end of a string?",
     Answer: ".trim()",
     Distractor1: ".substring()",
