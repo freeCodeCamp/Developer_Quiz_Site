e356c9b50465d2f18620278ed2a42951218f6d0b
Sarang-Goutam <107671448+Sarang-Goutam@users.noreply.github.com>
2022-10-01 23:20:27 +0530

    Update general-cs-quiz.ts
    For issue #29

--- src/data/general-cs-quiz.ts
+++ src/data/general-cs-quiz.ts
@@ -714,6 +714,15 @@ const generalCSQuiz = [
     Explanation: "SVG stands for Scalable Vector Graphics.",
     Link: "https://www.freecodecamp.org/news/use-svg-images-in-css-html/",
   },
+  {
+    Question: "Are JPEG and JPG sare the same?",
+    Answer: "Yes",
+    Distractor1: "May Be",
+    Distractor2: "No",
+    Distractor3: "Can't be said",
+    Explanation: "Yes - the only difference is that, traditionally, file extensions are only 3 characters long. '.jpg' is a shortened form of '.jpeg'.",
+    Link: "https://www.freecodecamp.org/news/image-file-types-picture-format-extensions-jpeg-gif-png-svg-tiff/",
+  },
 ];
 
 export default generalCSQuiz;
