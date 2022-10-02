128e59c20a0dabd6518db5b995ac3962667bc50a
John Anders <66134557+johnpatrickanders@users.noreply.github.com>
2022-10-02 16:18:07 -0400

    Update sql-quiz.ts

--- src/data/sql-quiz.ts
+++ src/data/sql-quiz.ts
@@ -355,6 +355,17 @@ const sqlQuiz = [
       "The NOW() function gives the selected record a value of the current timestamp",
     Link: "https://www.freecodecamp.org/news/sql-date-functions-getdate/",
   },
+  {
+    Question:
+      "In SQL, where can we access all column names within a database?",
+    Answer: "INFORMATION_SCHEMA.COLUMNS",
+    Distractor1: "COLUMNS",
+    Distractor2: "SCHEMA.COLUMNS",
+    Distractor3: "SCHEMA.INFO.COLS",
+    Explanation:
+      "You can select from INORMATION_SCHEMA.COLUMNS and combine with a WHERE clause when needing to identify where a certain column is stored.",
+    Link: "https://www.freecodecamp.org/news/learn-sql-queries-database-query-tutorial-for-beginners/",
+  },
 ];
 
 export default sqlQuiz;
