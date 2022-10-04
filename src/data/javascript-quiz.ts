030f62e88ec2984f0eb49d52949c7447fed4dc42
Joseph Odunsi <odunsiolakunbi@gmail.com>
2022-10-04 17:07:23 +0100

    chore: added javascript questions on type coercion and falsy values (#197)
    * add two JavaScript questions
    
    * change type coercion distractor no.2
    
    * requested changes
    
    * more javascript question on function
    
    * fix unintentional changes
    
    * fix: resolving merge conflict
    
    Co-authored-by: Jessica Wilkins <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/javascript-quiz.ts
+++ src/data/javascript-quiz.ts
@@ -1171,6 +1171,48 @@ const javascriptQuiz = [
       "Variables declared with let, const and var are hoisted. But, const and let are in temporal dead zone.",
     Link: "https://www.freecodecamp.org/news/javascript-temporal-dead-zone-and-hoisting-explained/",
   },
+  {
+    Question: "In JavaScript, what is the difference between == and ===?",
+    Answer: "== allows for type coercion of the values, while === does not",
+    Distractor1:
+      "== does not allow for type coercion of the values, while === does",
+    Distractor2:
+      "== and === both allow for type coercion of the values, but === is more strict",
+    Distractor3: "There is no difference between == and ===",
+    Explanation:
+      "The == operator allows for type coercion of the values, while === does not.",
+    Link: "https://www.freecodecamp.org/news/js-type-coercion-explained-27ba3d9a2839/",
+  },
+  {
+    Question: "In JavaScript, how many falsy values are there?",
+    Answer: "6",
+    Distractor1: "5",
+    Distractor2: "7",
+    Distractor3: "8",
+    Explanation:
+      "There are 6 falsy values in JavaScript: false, 0, '', null, undefined, and NaN.",
+    Link: "https://www.freecodecamp.org/news/falsy-values-in-javascript/",
+  },
+  {
+    Question: "In JavaScript, what is a function without a name called?",
+    Answer: "An anonymous function",
+    Distractor1: "An unsigned function",
+    Distractor2: "A ghost function",
+    Distractor3: "A nameless function",
+    Explanation:
+      "A function without a name is called an anonymous function. This is useful when you want to pass a function as an argument to another function.",
+    Link: "https://www.freecodecamp.org/news/what-are-functions-in-javascript-a-beginners-guide/",
+  },
+  {
+    Question: "In JavaScript, what does IIFE stand for?",
+    Answer: "Immediately Invoked Function Expression",
+    Distractor1: "Immediately Invoked Function Executor",
+    Distractor2: "Internally Invoked Function Expression",
+    Distractor3: "Internally Invoked Function Executor",
+    Explanation:
+      "IIFE stands for Immediately Invoked Function Expression. This is a function that is executed immediately after it is created.",
+    Link: "https://www.freecodecamp.org/news/iife-in-javascript-what/",
+  },
 ];
 
 export default javascriptQuiz;
