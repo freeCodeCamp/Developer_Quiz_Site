6fea5a92e5f0a696e8361475c8549067b6929c2a
Atharv Agarwal <89630019+atharvagarwal@users.noreply.github.com>
2022-10-06 20:31:05 +0530

    Update src/data/general-cs-quiz.ts
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/general-cs-quiz.ts
+++ src/data/general-cs-quiz.ts
@@ -805,7 +805,7 @@ const generalCSQuiz = [
     Distractor1: "O(n^2)",
     Distractor2: "O(log(n))",
     Distractor3: "O(2^n)",
-    Explanation: "The list of size N is divided into a max of Logn parts, and the merging of all sublists into a single list takes O(N) time,Best Case Time Complexity: O(n*log n)",
+    Explanation: "Merge sort is a divide and conquer algorithm that has a best case time complexity of O(n*log n)",
     Link: "https://www.freecodecamp.org/news/an-intro-to-advanced-sorting-algorithms-merge-quick-radix-sort-in-javascript-b65842194597/",
   },
 ];
