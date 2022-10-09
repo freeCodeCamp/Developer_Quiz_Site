f4bc25a149f2f4be6d9ba8352722dfbcb70b4fdd
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-10-10 00:35:46 +0600

    Update src/data/python-quiz.ts
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/python-quiz.ts
+++ src/data/python-quiz.ts
@@ -2,10 +2,10 @@ const pythonQuiz = [
    {
     Question:
       "In Python, which of the following options will reverse this string: sentence='freeCodeCamp rules'?",
-    Answer: "a_string[::-1]",
-    Distractor1: "reversed(a_string)",
-    Distractor2: "a_string.reverse()",
-    Distractor3: "a_string.sort(reverse=True)",
+    Answer: "sentence[::-1]",
+    Distractor1: "reversed(sentence)",
+    Distractor2: "sentence.reverse()",
+    Distractor3: "sentence.sort(reverse=True)",
     Explanation:
       "<string>[start: stop: step] returns a slice of the string. <string>[:::] returns a copy of the entire string. <string>[::-1] returns a reversed copy of the string.",
     Link: "https://www.freecodecamp.org/news/python-reverse-string-string-reversal-in-python-explained-with-code-examples/",
