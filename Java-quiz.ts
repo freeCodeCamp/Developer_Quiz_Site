2ec79292771c608cc0cdd8cc4ea1580934b6a344
manan-dude <76246911+manan-dude@users.noreply.github.com>
2022-10-09 22:46:59 +0530

    Update Java-quiz.ts

--- Java-quiz.ts
+++ Java-quiz.ts
@@ -1,11 +1,11 @@
  {
-    Question: 
-      "Java is a ____________ language?",
-    Answer: "not a Pure Object Oriented Language",
-    Distractor1: "not a Pure Object Oriented Language",
-    Distractor2: "Object Oriented Language",
-    Distractor3: "Structured Language",
-    Explanation: 
-      "because it uses primitive data types such as (int,float,char...etc).",
-    Link: "https://www.freecodecamp.org/news/tag/java/",
+    Question:
+      "In Python, which of the following option will reverse a_string='good' and make it 'doog'?",
+    Answer: "a_string[::-1]",
+    Distractor1: "reversed(a_string)",
+    Distractor2: "a_string.reverse()",
+    Distractor3: "a_string.sort(reverse=True)",
+    Explanation:
+      "<string>[start: stop: step] returns a slice of the string. <string>[:::] returns a copy of the entire string. <string>[::-1] returns a reversed copy of the string.",
+    Link: "https://www.freecodecamp.org/news/python-reverse-string-string-reversal-in-python-explained-with-code-examples/",
   },
