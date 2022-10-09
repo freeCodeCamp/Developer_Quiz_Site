1ad7505c4416bb7422268487e37e974af8a8d56c
manan-dude <76246911+manan-dude@users.noreply.github.com>
2022-10-09 22:24:53 +0530

    javaQue

---
+++ Java-quiz.ts
@@ -0,0 +1,9 @@
+ {
+    Question: "Java is a ____________ language?",
+    Answer: "not a Pure Object Oriented Language",
+    Distractor1: "not a Pure Object Oriented Language",
+    Distractor2: "Object Oriented Language",
+    Distractor3: "Structured Language",
+    Explanation: "because it uses primitive data types such as (int,float,char...etc).",
+    Link: "https://www.freecodecamp.org/news/tag/java/",
+  },
