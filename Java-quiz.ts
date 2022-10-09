3e2ca602675dc5c572046a5f22a8b84ac8506e3a
manan-dude <76246911+manan-dude@users.noreply.github.com>
2022-10-09 23:12:02 +0530

    Update Java-quiz.ts

--- Java-quiz.ts
+++ Java-quiz.ts
@@ -1,9 +1,13 @@
- {
+const Java-quiz =[ 
+{
     Question: "Java is a ____________ language?",
-    Answer: "not a Pure Object Oriented Language";
+    Answer: "not a Pure Object Oriented Language",
     Distractor1: "not a Pure Object Oriented Language",
     Distractor2: "Object Oriented Language",
     Distractor3: "Structured Language",
     Explanation: "because it uses primitive data types such as (int,float,char...etc).",
     Link: "https://www.freecodecamp.org/news/tag/java/",
   },
+];
+
+export default Java-quiz;
