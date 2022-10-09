15e03c4362871b0ed3e0b0327f857a1cb0ae9351
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-10-09 22:28:16 +0600

    Add a quiz question in python-quiz.ts

--- src/data/python-quiz.ts
+++ src/data/python-quiz.ts
@@ -1,4 +1,15 @@
 const pythonQuiz = [
+   {
+    Question:
+      "In Python, which of the following option will reverse a_string='good' and make it 'doog'?",
+    Answer: "a_string[::-1]",
+    Distractor1: "reversed(a_string)",
+    Distractor2: "a_string.reverse()",
+    Distractor3: "a_string.sort(reverse=True)",
+    Explanation:
+      "<string>[start: stop: step] returns a slice of the string. <string>[:::] returns a copy of the entire string. <string>[::-1] returns a reversed copy of the string.",
+    Link: "https://www.freecodecamp.org/news/python-reverse-string-string-reversal-in-python-explained-with-code-examples/",
+  },
   {
     Question: 
       "In Python, which module provides a framework for writing line-oriented command interpreters?",
