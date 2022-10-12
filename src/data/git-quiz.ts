fe1e3a8d5de2ce67e3f0334c4ac393e5f23b15d0
Makdoom Shaikh <makshaikh99@gmail.com>
2022-10-13 00:33:37 +0530

    feat: add two new git quiz question (#322)
    * feat: add two new git quiz question
    
    * fix: removing extra spaces
    
    * fix: grammar for additional question
    
    Co-authored-by: Jessica Wilkins <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/git-quiz.ts
+++ src/data/git-quiz.ts
@@ -11,8 +11,7 @@ const gitQuiz = [
     Link: "https://www.freecodecamp.org/news/how-to-use-git-stash-as-temporary-storage-84a0a1e37a43/",
   },
   {
-    Question:
-      "In Git, how can you check your configuration?",
+    Question: "In Git, how can you check your configuration?",
     Answer: "git config -l",
     Distractor1: "git config -check",
     Distractor2: "git checkconfig -l",
@@ -367,7 +366,8 @@ const gitQuiz = [
     Link: "https://www.freecodecamp.org/news/10-important-git-commands-that-every-developer-should-know/",
   },
   {
-    Question: "In Git, which command is used to reset a branch to a previous commit?",
+    Question:
+      "In Git, which command is used to reset a branch to a previous commit?",
     Answer: "git reset",
     Distractor1: "git revert",
     Distractor2: "git rebase",
@@ -375,6 +375,28 @@ const gitQuiz = [
     Explanation:
       "The git reset command is used to reset your current HEAD to a specified state.",
     Link: "https://www.freecodecamp.org/news/the-ultimate-guide-to-git-reset-and-git-revert/",
+  },
+  {
+    Question:
+      "In Git, how do you see your commit history including changes in files?",
+    Answer: "git log -p",
+    Distractor1: "git log --show",
+    Distractor2: "git log -c",
+    Distractor3: "git log",
+    Explanation:
+      "The git log -p command shows the commit's history including all files and their changes.",
+    Link: "https://www.freecodecamp.org/news/git-cheat-sheet/",
+  },
+  {
+    Question:
+      "In Git, which command is used to list all of the remote branches that Git is tracking?",
+    Answer: "git branch -r",
+    Distractor1: "git branch --show -r",
+    Distractor2: "git -b -d",
+    Distractor3: "git branch",
+    Explanation:
+      "The git branch -r command shows the name of all remote branches that Git is tracking for the current repository.",
+    Link: "https://www.freecodecamp.org/news/git-cheat-sheet/",
   },
 ];
 
