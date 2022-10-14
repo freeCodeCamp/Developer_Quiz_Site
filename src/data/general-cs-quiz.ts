f14cd252d41d5fde1c34ad42731dc7fcd54b275d
Erick Adderly Leon Tejeda <39746929+erickleon88@users.noreply.github.com>
2022-10-14 00:10:33 -0500

    chore: adding question on OOP concepts (#366)
    * Html Question added by erickleon88
    
    * New Question to CS added
    
    * fix: resolving syntax errors
    
    * Update src/data/general-cs-quiz.ts
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>
    
    * Update src/data/general-cs-quiz.ts
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>
    
    * Update src/data/general-cs-quiz.ts
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>
    
    * Update src/data/general-cs-quiz.ts
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>
    
    * Resolved Questions Issues
    
    * Changes done
    
    * New Question to CS
    
    * New Question to CS
    
    * Changes done
    
    * fix: grammar in distractors
    
    Co-authored-by: Jessica Wilkins <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/general-cs-quiz.ts
+++ src/data/general-cs-quiz.ts
@@ -1,5 +1,14 @@
 const generalCSQuiz = [
   {
+    Question: "What is Object-Oriented Programming?",
+    Answer: "OOP is a programming pattern that is built around objects or entities.",
+    Distractor1: "OOP is a topological paradigm to program things",
+    Distractor2: "OOP is a set of sorting algorithms",
+    Distractor3: "OOP is a list of Bitwise operators",
+    Explanation:"OOP is a programming pattern that is built around objects or entities",
+    Link: "https://www.freecodecamp.org/news/what-is-object-oriented-programming/"
+  },
+  {
     Question: "Which term is used for removing elements from the front of the queue?",
     Answer: "Dequeue.",
     Distractor1: "Enqueue.",
@@ -199,7 +208,7 @@ const generalCSQuiz = [
       "Big O notation describes the upper bound. The function n upper bounds the function 2*logn. However, it is not the tightest upper bound, and in general we always specify the tightest upper bound to quantify an algorithm.",
     Link: "https://www.youtube.com/watch?v=RBSGKlAvoiM&list=PLWKjhJtqVAbn5emQ3RRG8gEBqkhf_5vxD&t=5m43s",
   },
-   {
+  {
     Question:
       "Between Merge sort and Quicksort, which sorting algorithm works faster in the case of larger datasets?",
     Answer: "Merge sort",
