d089ee01bfaa5b1af524fefbd1cd964746cb079d
preeformance <93346315+preeformance@users.noreply.github.com>
2022-10-14 16:26:19 -0400

    Update accessibility-quiz.ts

--- src/data/accessibility-quiz.ts
+++ src/data/accessibility-quiz.ts
@@ -1,4 +1,14 @@
 const accessibilityQuiz = [
+   {
+    Question: "What attribute makes images accessible for people using screen readers?",
+    Answer: "alt",
+    Distractor1: "desc",
+    Distractor2: "img",
+    Distractor3: "src",
+    Explanation:
+      "With one attribute, alt, we are able to make the image accessible for screen reader users.",
+    Link: "https://www.freecodecamp.org/news/common-accessibility-errors-and-how-to-fix-them/",
+  },
   {
     Question: "What is web accessibility?",
     Answer: "the practice of building websites that are usable by everyone",
