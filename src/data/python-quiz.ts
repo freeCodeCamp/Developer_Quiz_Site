40240b5dac0dfb1de86cd282b1fd94b676acf04a
Abhishek Mishra <77018574+abhishekiiitr@users.noreply.github.com>
2022-10-14 16:09:19 +0530

    Added a new Question
    Type of variable in Python

--- src/data/python-quiz.ts
+++ src/data/python-quiz.ts
@@ -1,4 +1,15 @@
 const pythonQuiz = [
+      {
+    Question: 
+      "In Python,if we have a='abhi' , then what is the result here : print(type(a))?",
+    Answer: "<class 'str'>",
+    Distractor1: "<class 'char'>",
+    Distractor2: "<class 'var'>",
+    Distractor3: "error",
+    Explanation: 
+      "type() method returns class type of the argument(object) passed as parameter in Python",
+    Link: "https://www.freecodecamp.org/news/python-print-type-of-variable-how-to-get-var-type/",
+  },
      {
     Question: 
       "In Python, if we have list=['rahul', 'monica', 'george', 'ross', 'george', 'ross'], then what is the result here: print(len(set(list))-len(list))?",
