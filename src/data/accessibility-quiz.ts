f0a99484d4adfe50ccfddaba404d58ac2b6d5fd3
Adarsh Gupta <73733229+adarsh-gupta101@users.noreply.github.com>
2022-10-15 11:14:04 +0530

    chore: Added a new question in web accessibility section (#378)
    * Added a new question in web accessibility section
    
    Added a question about semantic html for accessibility
    
    * Formmated question
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>
    
    * formmated answer
    
    added meaningful answer(from suggestion)
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/accessibility-quiz.ts
+++ src/data/accessibility-quiz.ts
@@ -19,6 +19,16 @@ const accessibilityQuiz = [
       "Web accessibility means building websites that are usable by as many people as possible.",
     Link: "https://www.freecodecamp.org/news/what-is-website-accessibility-18ce00ec990f/",
   },
+   {
+    Question: "Which of the following HTML elements can improve the accessibility for your website?",
+    Answer: "<section>",
+    Distractor1: "<div>",
+    Distractor2: "<span>",
+    Distractor3: "<head>",
+    Explanation:
+      "The section element holds semantic meaning and defines sections within the HTML document.",
+    Link: "https://www.freecodecamp.org/news/semantic-html5-elements/",
+  },
 ];
 
 export default accessibilityQuiz;
