3c1f7fa155a2b07981d94490d345a7f7c8930b85
Abhishek Mishra <77018574+abhishekiiitr@users.noreply.github.com>
2022-10-15 10:00:59 +0530

    Update python-quiz.ts

--- src/data/python-quiz.ts
+++ src/data/python-quiz.ts
@@ -1,13 +1,13 @@
 const pythonQuiz = [
       {
     Question: 
-      "In Python,if we have a='abhi' , then what is the result here : print(type(a))?",
+      "In Python,if organization='freeCodeCamp', then what is the result here : print(type(organization))?",
     Answer: "<class 'str'>",
     Distractor1: "<class 'char'>",
     Distractor2: "<class 'var'>",
     Distractor3: "error",
     Explanation: 
-      "type() method returns class type of the argument(object) passed as parameter in Python",
+      "Python's type() method returns the class type for the objects.",
     Link: "https://www.freecodecamp.org/news/python-print-type-of-variable-how-to-get-var-type/",
   },
      {
