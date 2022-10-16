21819ed0f21c7ae51671a84e53e77d08d43f737e
Shubhashish Sinha <40638763+shubhsinha@users.noreply.github.com>
2022-10-17 01:42:41 +0530

    added a question in linux-quiz.ts

--- src/data/linux-quiz.ts
+++ src/data/linux-quiz.ts
@@ -921,6 +921,16 @@ const linuxQuiz = [
       "The find command can be used to find files or folders matching a particular search pattern. It searches recursively.",
     Link: "https://www.freecodecamp.org/news/the-linux-commands-handbook/",
   },
+  {
+  Question: "Which of the following commands is used to calculate a file's total lines, words, and characters?",
+    Answer: "wc",
+    Distractor1: "wordcount",
+    Distractor2: "wordc",
+    Distractor3: "lc",
+    Explanation:
+      "A file's total number of lines, words, and characters can be determined with the wc command.",
+    Link: "https://www.freecodecamp.org/news/the-linux-commands-handbook/",
+  },
 ];
 
 export default linuxQuiz;
