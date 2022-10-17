7ccb2f496567ed0dbd0ac191ab7b18965695ef29
Shubhashish Sinha <40638763+shubhsinha@users.noreply.github.com>
2022-10-17 14:07:00 +0530

    Update src/data/sql-quiz.ts
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/sql-quiz.ts
+++ src/data/sql-quiz.ts
@@ -578,7 +578,7 @@ const sqlQuiz = [
   },
   {
     Question:
-      "The best datatype to use to store a string with up to 255 characters is which of the following?",
+      "In SQL, which data type is used to store a string with up to 255 characters?",
     Answer: "TINY TEST",
     Distractor1: "STRING",
     Distractor2: "BLOB",
