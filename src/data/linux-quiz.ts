806262ef66f0ed9220011c5176d69ea9cb1f4d84
PranjalSK03 <96343659+PranjalSK03@users.noreply.github.com>
2022-10-18 11:25:12 +0530

    chore: added new question on chmod in linux (#444)
    * use of comment to make css look better
    
    dividing into section for better readability
    
    * added a new question on chmod implementation
    
    * made the required changes on chmod question
    
    * removed the commnent as asked
    
    * changed the required resource

--- src/data/linux-quiz.ts
+++ src/data/linux-quiz.ts
@@ -1,5 +1,17 @@
 const linuxQuiz = [
   {
+    
+    Question: "In Linux, which command is used to remove the writing and executing permissions for 'group' and 'other'?",
+    Answer: "chmod go-wx list",
+    Distractor1: "chmod go+wx list",
+    Distractor2: "chmod a-wx list",
+    Distractor3: "chmod go+r list",
+    Explanation:
+      "The 'chmod go-wx list' is used to remove the writing and executing permissions for 'group' and 'other' for a file named list.",
+    Link: "https://www.freecodecamp.org/news/linux-chmod-chown-change-file-permissions/",
+  },
+  {
+    
     Question: "In Linux, which one of the following commands determines file type?",
     Answer: "file",
     Distractor1: "kind",
