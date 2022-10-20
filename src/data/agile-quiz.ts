99f576bd4bb605d136e77672c48d9299faefd04b
Camilo Cortes <67564515+CamiloCortesM@users.noreply.github.com>
2022-10-20 00:57:38 -0500

    add new Agile question

--- src/data/agile-quiz.ts
+++ src/data/agile-quiz.ts
@@ -279,6 +279,16 @@ const agileQuiz = [
       "Feature Driven Development (FDD) is the practice of incrementally developing and frequently releasing new features for software products.",
     Link: "https://www.freecodecamp.org/news/complete-guide-to-agile-methodology/"
   },
+  {
+    Question: "In Agile, Which agile methodology has no defined roles or functions and each member can assume the available tasks?",
+    Answer: "Kanban",
+    Distractor1: "Scrum",
+    Distractor2: "Lean",
+    Distractor3: "Agile Inception ",
+    Explanation:
+      "The Kanban method does not prescribe roles or meetings as do other frameworks such as Scrum. However, roles and meetings have been identified from experience in organizations that are relevant for the organization to follow the principles of the method through its practices..",
+    Link: "https://www.freecodecamp.org/news/being-agile-kanban-vs-scrum/"
+  },
 ];
 
 export default agileQuiz;
