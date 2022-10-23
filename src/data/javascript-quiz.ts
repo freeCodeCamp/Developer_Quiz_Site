c80d51089e3a53ad28cc27aba708290fa3df74c1
Kristen <102562797+KristenHarman@users.noreply.github.com>
2022-10-23 11:55:00 -0500

    Update src/data/javascript-quiz.ts
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/javascript-quiz.ts
+++ src/data/javascript-quiz.ts
@@ -1792,7 +1792,7 @@ const javascriptQuiz = [
     Distractor2: "charAt(0)",
     Distractor3: "getPosition()",
     Explanation:
-      "The lastIndexOf() method returns the last index at which a given element can be found in the array, or -1 if it is not present. The array is searched backwards, starting at fromIndex.",
+      "The lastIndexOf() method returns the last index at which a given element can be found in the array, or -1 if it is not present.",
     Link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/lastIndexOf"
   }
 ];
