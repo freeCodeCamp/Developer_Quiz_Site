33a48c8e60df2648159272b994658573cfa729ed
Sharoon Rafeek <sharoonrafeek@gmail.com>
2022-10-23 17:14:20 +0530

    Added python quiz question

--- src/data/python-quiz.ts
+++ src/data/python-quiz.ts
@@ -1,6 +1,17 @@
 const pythonQuiz = [
    {
     Question: 
+      "In Python, what is the output of the following code print(list(range(3, 6)))?",
+    Answer: "[3, 4, 5]",
+    Distractor1: "[3, 4, 5, 6]",
+    Distractor2: "[3, 6]",
+    Distractor3: "[3, 3, 3, 3, 3, 3]",
+    Explanation: 
+      "The list(range(3, 6)) function returns a list of numbers from 3, increments by 1 (by default), and stops at 5 (excluding 6).",
+    Link: "https://www.freecodecamp.org/news/python-range-function-explained-with-code-examples/",
+   },
+   {
+    Question: 
       "In Python, which method is used to write text to a file?",
     Answer: "write()",
     Distractor1: "read()",
