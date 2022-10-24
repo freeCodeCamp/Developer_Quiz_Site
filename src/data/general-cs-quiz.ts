fb0945c6eab5642b427f8f3426c5916a5422a60e
Jessica Wilkins <67210629+jdwilkin4@users.noreply.github.com>
2022-10-23 19:11:15 -0700

    fix: grammar

--- src/data/general-cs-quiz.ts
+++ src/data/general-cs-quiz.ts
@@ -1283,14 +1283,13 @@ const generalCSQuiz = [
     Link: "https://www.freecodecamp.org/news/learn-big-o-notation/"
   },
   {
-    
     Question: "What is the worst case time complexity for binary and linear search?",
     Answer: "binary search is O(logn) and linear search is O(n)",
     Distractor1: "binary search is O(n) and linear search is O(logn)",
     Distractor2: "binary search is O(n) and linear search is O(n)",
     Distractor3: "binary search is O(logn) and linear search is O(logn)",
     Explanation:
-      "The worst case time complexity for linear search O(n) because we have iterate through n elements. The worst case time complexity for binary search is O(logn) because it is a divide and conquer algorithm.",
+      "The worst case time complexity for linear search O(n) because we have to iterate through n elements. The worst case time complexity for binary search is O(logn) because it is a divide and conquer algorithm.",
     Link: "https://www.freecodecamp.org/news/search-algorithms-linear-and-binary-search-explained/"
   },
 ];
