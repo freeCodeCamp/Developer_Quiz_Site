995e6d17db15b30179c99031a4ca129e4575b2a6
Vishal Yadav <vishalyadav1746@gmail.com>
2022-10-24 14:35:14 +0530

    Added question in linux-quiz.ts

--- src/data/linux-quiz.ts
+++ src/data/linux-quiz.ts
@@ -1,5 +1,14 @@
 const linuxQuiz = [
   {
+    Question: "In Linux, which one of the following commands can be used to create a symbolic link between files?",
+    Answer: "ln",
+    Distractor1: "ls",
+    Distractor2: "ln",
+    Distractor3: "link",
+    Explanation: "The command 'ln' creates a link to TARGET with the name LINK_NAME.",
+    Link: "https://man7.org/linux/man-pages/man1/ln.1.html"
+  },
+  {
     Question: "Which command finds memory being used by Linux?",
     Answer: "All of these",
     Distractor1: "free-m",
