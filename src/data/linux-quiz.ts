8ca546c91f11240c2d282ec68b5ebe5758974240
Tripura kant <tripura.kan@gmail.com>
2022-10-25 09:55:19 +0530

    new git question added

--- src/data/linux-quiz.ts
+++ src/data/linux-quiz.ts
@@ -1,5 +1,16 @@
 const linuxQuiz = [
   {
+    Question:
+      "In Git, how can you stage a file that ends with a *.txt extension?",
+    Answer: "git add *.txt",
+    Distractor1: "git add *.text",
+    Distractor2: "git add -a .txt",
+    Distractor3: "git add - *.txt",
+    Explanation:
+      "git add is a command used to add a file that is in the working directory to the staging area. Use *.txt to add specific pattern.",
+    Link: "https://www.freecodecamp.org/news/learn-the-basics-of-git-in-under-10-minutes-da548267cc91/"
+  },
+  {
     Question: "Which command finds memory being used by Linux?",
     Answer: "All of these",
     Distractor1: "free-m",
