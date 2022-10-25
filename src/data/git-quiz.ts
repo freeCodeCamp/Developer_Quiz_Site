a0d973fcaf1285bbea0993145350a479834cfc88
Tripura kant <tripurakant@gmail.com>
2022-10-25 09:50:45 +0530

    chore: Added question in git Quiz (#546)
    * added question in git
    
    * Update src/data/git-quiz.ts
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>
    
    * Update git-quiz.ts
    
    * added question in git
    
    Co-authored-by: Tripura kant <tripura.kan@gmail.com>
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/git-quiz.ts
+++ src/data/git-quiz.ts
@@ -1,6 +1,17 @@
 const gitQuiz = [
   {
     Question:
+      "In Git, how can you stage a file that ends with a *.txt extension?",
+    Answer: "git add *.txt",
+    Distractor1: "git add *.text",
+    Distractor2: "git add -a .txt",
+    Distractor3: "git add - *.txt",
+    Explanation:
+      "git add is a command used to add a file that is in the working directory to the staging area. Use *.txt to add specific pattern.",
+    Link: "https://www.freecodecamp.org/news/learn-the-basics-of-git-in-under-10-minutes-da548267cc91/"
+  },
+  {
+    Question:
       "In Git, how can you save uncommitted changes on your local machine so you can work on it later?",
     Answer: "git stash",
     Distractor1: "git copy into notepad.exe",
