7ac3ef7421887b247705c9242bc8f921c3b8b1b1
Chirag Arora <84070677+Chirag8023@users.noreply.github.com>
2022-10-29 01:13:24 +0530

    chore: adding new question on proprietary software (#577)
    * Update general-cs-quiz.ts
    
    * Update src/data/general-cs-quiz.ts
    
    * Update src/data/general-cs-quiz.ts
    
    * Update src/data/general-cs-quiz.ts
    
    * Update src/data/general-cs-quiz.ts
    
    * Update src/data/general-cs-quiz.ts
    
    Co-authored-by: Jessica Wilkins <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/general-cs-quiz.ts
+++ src/data/general-cs-quiz.ts
@@ -1321,6 +1321,15 @@ const generalCSQuiz = [
     Link: "https://www.freecodecamp.org/news/search-algorithms-linear-and-binary-search-explained/"
   },
   {
+    Question:"What is Proprietary Software?",
+    Answer: "Software which is owned by an organization or an individual.",
+    Distractor1: "Software where the code is publicly available for people to modify, and share.",
+    Distractor2: "Software where the code can be used by everyone for free",
+    Distractor3: "Software that has only been developed by Oracle",
+    Explanation: "The original authors of proprietary software can modify its code. Users of this type of software have to agree not to alter it in any way unless they have permission from the owners to do so.",
+    Link: "https://www.freecodecamp.org/news/what-is-open-source-software-explained-in-plain-english"
+  },
+  {
     Question:"What is Open Source Software?",
     Answer: "A type of software where the code is publicly available for people to view, modify, and share.",
     Distractor1: "Software owned by Big Tech Companies",
