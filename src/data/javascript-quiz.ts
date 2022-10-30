1f2ca9b95392832fbee3f3badf98998cb14428e1
shubhshish <116957065+shubhshish@users.noreply.github.com>
2022-10-30 21:13:43 +0530

    Update src/data/javascript-quiz.ts
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/javascript-quiz.ts
+++ src/data/javascript-quiz.ts
@@ -1959,7 +1959,7 @@ const javascriptQuiz = [
     Link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach"
   },
 {
-    Question: "In JavaScript, what is a variable that can be changed called?",
+    Question: "In JavaScript, what is the name for a variable that can be changed?",
     Answer: "Mutable Variable",
     Distractor1: "Volatile Variable",
     Distractor2: "Dynamic Variable",
