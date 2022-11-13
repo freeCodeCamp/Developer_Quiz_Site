ffcd34aa85c36e52242307dd039b1084a4a0e7ee
Shaik Mohammed Huzaifa <shaikmohdhuz@gmail.com>
2022-11-13 23:03:19 +0530

    Update src/data/css-quiz.ts
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/css-quiz.ts
+++ src/data/css-quiz.ts
@@ -1248,7 +1248,7 @@ const cssQuiz = [
     Answer: "flex-grow, flex-shrink, and flex-basis",
     Distractor1: "justify-content, align-items, and align-content",
     Distractor2: "flex-direction, flex-wrap, and flex-flow",
-    Distractor3: "flex-direction, flex-wrap, and flex-flow",
+    Distractor3: "flex-direction, order, and flex-reverse",
     Explanation:
       "Flex is a shorthand property for these three properties: flex-grow, flex-shrink, and flex-basis.",
     Link: "https://developer.mozilla.org/en-US/docs/Web/CSS/flex"
