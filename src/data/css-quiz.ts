acf3aab6e8c7129b4eddd5ffea1f86e6c4738352
Shaik Mohammed Huzaifa <shaikmohdhuz@gmail.com>
2022-11-15 12:47:07 +0530

    adding css question for flexbox(#634)
    * Add a linux question about if statements
    
    * Add new Css question about flex shorthand
    
    * Update src/data/css-quiz.ts
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>
    
    * Update src/data/linux-quiz.ts
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>
    
    * Update src/data/css-quiz.ts
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>
    
    * Update src/data/linux-quiz.ts
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>
    
    * Add FreeCodeCamp link
    
    * Add the bash script article link from Freecodecamp
    
    * fix: undo deletions from merge conflict
    
    * fix: adding in css flex question
    
    * fix: distractor answer
    
    * fix: grammar
    
    Co-authored-by: Jessica Wilkins  <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/css-quiz.ts
+++ src/data/css-quiz.ts
@@ -1,6 +1,17 @@
 const cssQuiz = [
   {
     Question:
+      "In CSS, Flex is a shorthand property for which of the following properties?",
+    Answer: "flex-grow, flex-shrink, and flex-basis",
+    Distractor1: "justify-content, align-items, and align-content",
+    Distractor2: "flex-direction, flex-wrap, and flex-flow",
+    Distractor3: "flex-direction, order, and flex-flow",
+    Explanation:
+      "Flex is a shorthand property for all three of these properties: flex-grow, flex-shrink, and flex-basis.",
+    Link: "https://developer.mozilla.org/en-US/docs/Web/CSS/flex"
+  },
+  {
+    Question:
       "Which CSS property is used to represent the transparency of an element?",
     Answer: "Opacity",
     Distractor1: "Dark",
