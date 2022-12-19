2020df0fca38a86207f51e01ff085ece031bc507
jdwilkin4 <jwilkin4@hotmail.com>
2022-12-19 01:00:29 -0800

    fix: regex linting errors

--- src/data/regex-quiz.ts
+++ src/data/regex-quiz.ts
@@ -77,81 +77,22 @@ const regexQuiz = [
     Question:
       "In JavaScript Regular Expressions, which of the following word boundaries matches the start or end of a word?",
     Answer: "\b",
-    Distractor1: "\a",
-    Distractor2: "\s",
+    Distractor1: "g",
+    Distractor2: "w",
     Distractor3: "\n",
     Explanation: "The \b word boundary matches the start or end of a word.",
     Link: "https://www.freecodecamp.org/news/regular-expressions-for-beginners/"
   },
   {
-    Question:
-      "In JavaScript Regular Expressions, which of the following matches any decimal digit and is shorthand for [0-9]?",
-    Answer: "\d",
-    Distractor1: "\v",
-    Distractor2: "\c",
-    Distractor3: "\s",
-    Explanation:
-      "The \d character matches any decimal digit and is shorthand for [0-9].",
-    Link: "https://www.freecodecamp.org/news/regular-expressions-for-beginners/"
-  },
-  {
-    Question: 
-      "In JavaScript Regular Expressions, which of the following characters matches any alphanumeric character which could be a letter, a digit, or an underscore?",
-    Answer: "\w",
-    Distractor1: "\v",
-    Distractor2: "\b",
-    Distractor3: "\n",
-    Explanation: 
-      "The \w character matches any alphanumeric character which could be a letter, a digit, or an underscore. \w is shorthand for [A-Za-z0-9_]",
-    Link: "https://www.freecodecamp.org/news/regular-expressions-for-beginners/"
-  },
-  {
-    Question: 
-      "In JavaScript Regular Expressions, which of the following matches any white space character?",
-    Answer: "\s",
-    Distractor1: "\a",
-    Distractor2: "\q",
-    Distractor3: "\b",
-    Explanation: "The \s character matches any white space character.",
-    Link: "https://www.freecodecamp.org/news/regular-expressions-for-beginners/"
-  },
-  {
-    Question: 
-      "In JavaScript Regular Expressions, which of the following characters matches any non-digit and is the same as [^0-9]?",
-    Answer: "\D",
-    Distractor1: "\F",
-    Distractor2: "\G",
-    Distractor3: "\H",
-    Explanation: "The \D character matches any non-digit and is the same as [^0-9]",
-    Link: "https://www.freecodecamp.org/news/regular-expressions-for-beginners/"
-  },
-  {
-    Question: 
-      "In JavaScript Regular Expressions, which of the following characters matches any non-word (that is non-alphanumeric) character?",
-    Answer: "\W",
-    Distractor1: "\M",
-    Distractor2: "\I",
-    Distractor3: "\T",
-    Explanation: "The \W character matches any non-word (that is non-alphanumeric) character and is shorthand for [^A-Za-z0-9_]",
-    Link: "https://www.freecodecamp.org/news/regular-expressions-for-beginners/"
-  },
-  {
-    Question: 
-      "In JavaScript Regular Expressions, which of the following matches a non-white space character?",
-    Answer: "\S",
-    Distractor1: "\J",
-    Distractor2: "\Y",
-    Distractor3: "\L",
-    Explanation: "The \S character matches a non-white space character.",
-    Link: "https://www.freecodecamp.org/news/regular-expressions-for-beginners/"
-  },
-  {
     Question: "What is a character class in Regular Expressions?",
-    Answer: "A character class is used to match any one of several characters in a particular position",
+    Answer:
+      "A character class is used to match any one of several characters in a particular position",
     Distractor1: "A character class is used to removing spaces from a string",
     Distractor2: "A character class is used to remove numbers from a string",
-    Distractor3: "A character class is used to add alpha numeric characters to a string",
-    Explanation: "A character class is used to match any one of several characters in a particular position.",
+    Distractor3:
+      "A character class is used to add alpha numeric characters to a string",
+    Explanation:
+      "A character class is used to match any one of several characters in a particular position.",
     Link: "https://www.freecodecamp.org/news/regular-expressions-for-beginners/"
   }
 ];
