d4a478129786b2131c810557bc7816c8bf91cfec
Rishav Sachdeva <rishu.sach9911@gmail.com>
2023-10-03 00:25:16 +0530

    Added an accessibility question

--- src/data/accessibility-quiz.ts
+++ src/data/accessibility-quiz.ts
@@ -8,16 +8,7 @@ const accessibilityQuiz = [
     Explanation: "Use the 'em' unit to set font sizes.Use relative length units and percentages",
     Link: "https://www.w3.org/WAI/GL/WD-WCAG10-TECHS-20000615/css-techniques.html#:~:text=Users%20should%20where%20necessary.,units%20even%20in%20absolute%20positioning."
   },
-  {
-    Question: "What does WCAG (sometimes pronounced wee-kag) stand for?",
-    Answer: "Web Content Accessibility Guidelines",
-    Distractor1: "World Content Accessibility Guidelines",
-    Distractor2: "Web Consortium Accessibility Guidelines",
-    Distractor3: "World Consortium Accessibility Guidelines",
-    Explanation: "Web Content Accessibility Guidelines (WCAG) covers a wide range of recommendations for making Web content more accessible.",
-    Link: "https://www.freecodecamp.org/news/what-is-website-accessibility-18ce00ec990f/"
-  },
-     {
+    {
     Question: "What is the purpose of the 'aria-expanded' attribute in ARIA?",
     Answer:
       "To indicate whether an element is open or closed when the visual indicator is not available",
