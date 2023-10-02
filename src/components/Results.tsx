d9f7a89494356647dfb73f291317591c16e41d27
Gautam-Hegde <gautamatjob@protonmail.com>
2023-10-02 19:46:46 +0530

    feat: add confetti animation

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Confetti from "react-confetti";
 
 interface PointTotals {
   points: number;
@@ -14,9 +15,52 @@ const Results: React.FC<PointTotals> = ({
   const totalPercentageCorrect = (Math.floor(points) / totalQuestions) * 100;
   const tweetMessage = `http://twitter.com/intent/tweet?text=I just scored ${totalPercentageCorrect}%25 on developerquiz.org. Wanna try it for yourself?&hashtags=freecodecamp`;
 
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  useEffect(() => {
+    // Update window size when it changes
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener on unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (points === totalQuestions) {
+      setShowConfetti(true);
+
+      // Remove the confetti after 5 seconds
+      const confettiTimeout = setTimeout(() => {
+        setShowConfetti(false);
+      }, 5000);
+
+      // Clean up the timeout on unmount
+      return () => {
+        clearTimeout(confettiTimeout);
+      };
+    }
+  }, [points, totalQuestions]);
+
   return (
     <div className="results-div">
       <h1 className="results-heading">Results</h1>
+      {showConfetti && (
+        <Confetti width={windowSize.width} height={windowSize.height} />
+      )}
       <h2>
         {points === totalQuestions ? "Wow! Perfect Score!" : "You received"}{" "}
         {points} out of {totalQuestions} points
