4015ef57b08111c502f1358ece240be3da5ead03
Saurav Srivastava <srivastava2000saurav@gmail.com>
2023-10-04 04:53:50 +0530

    chore: Added a devops question. (#732)

--- src/data/devops-quiz.ts
+++ src/data/devops-quiz.ts
@@ -1,6 +1,18 @@
 const devopsQuiz = [
   {
     Question:
+      "What are Docker images built out of?",
+    Answer:
+      "Filesystem layers,",
+    Distractor1: "Operating Systems",
+    Distractor2: "Virtual Machines",
+    Distractor3: "Containers",
+    Explanation:
+      "When you build a Docker image, each instruction in the Dockerfile contributes to a new layer. For example, if you have an instruction to install a software package, it creates a layer with the changes related to that installation.",
+    Link: "https://www.freecodecamp.org/news/docker-devops-course/"
+  },
+  {
+    Question:
       "Within the realm of DevOps, what does 'Immutable Infrastructure' signify?",
     Answer:
       "An approach where infrastructure components are never modified but replaced entirely when changes are needed",
