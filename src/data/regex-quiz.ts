192285d82e126e752c2c7750e04b8eaecc5c5046
Sahil Bheke <90205749+sahilbheke@users.noreply.github.com>
2023-10-09 03:17:30 +0530

    chore: adding regex question on quantifiers (#766)
    * Update regex-quiz.ts
    
    Added a new question.
    
    * Update regex-quiz.ts
    
    Added a new question.
    
    * Update regex-quiz.ts
    
    removed
    
    * Update regex-quiz.ts
    
    Added a new Question.

--- src/data/regex-quiz.ts
+++ src/data/regex-quiz.ts
@@ -1,5 +1,14 @@
 const regexQuiz = [
   {
+    Question: "Which regex quantifier matches 0 or 1 occurrence of the preceding character or group?",
+    Answer: "?",
+    Distractor1: "*",
+    Distractor2: "+",
+    Distractor3: "{3}",
+    Explanation: "The ? quantifier matches the preceding character or group 0 or 1 time, indicating that it is optional.",
+    Link: "https://www.freecodecamp.org/news/practical-regex-guide-with-real-life-examples/"
+  },
+  {
     Question: "What does the regex character class [A-Za-z] match?",
     Answer: "Any letter(both uppercase and lowercase)",
     Distractor1: "Any number",
