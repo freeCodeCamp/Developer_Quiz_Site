b87257aeadc1ec24c8b6fe044b65612265243154
Aditya <114663382+AaadityaG@users.noreply.github.com>
2023-10-12 09:16:47 +0530

    Update accessibility-quiz.ts

--- src/data/accessibility-quiz.ts
+++ src/data/accessibility-quiz.ts
@@ -530,16 +530,6 @@ const accessibilityQuiz = [
       "The four broad guidelines of web accessibility are Perceivable, Operable, Understandable, Robust. They help ensure that web content is accessible to all users, including those with disabilities",
     Link: "https://www.freecodecamp.org/news/what-is-website-accessibility-18ce00ec990f/"
   },
-   {
-    Question: "What is web accessibility?",
-    Answer: "the practice of building websites that are usable by everyone",
-    Distractor1: "a new JavaScript framework",
-    Distractor2: "a sorting algorithm",
-    Distractor3: "a type of programming paradigm",
-    Explanation:
-      "Web accessibility means building websites that are usable by as many people as possible.",
-    Link: "https://www.freecodecamp.org/news/what-is-website-accessibility-18ce00ec990f/",
-  },
   {
     Question: "Which of the following is NOT the way to fix web accessibility errors?",
     Answer: "Changing document color",
@@ -547,7 +537,7 @@ const accessibilityQuiz = [
     Distractor2: "Adding Missing Alternative Text for Images",
     Distractor3: "Fix Empty Links",
     Explanation:
-      "Fix Empty Links, Add Missing Alternative Text for Images and Update Low Contrast Text are the common ways to fix the accessiblility errorf",
+      "Fix Empty Links, Adding Missing Alternative Text for Images and Update Low Contrast Text are the common ways to fix the accessiblility error",
     Link: "https://www.freecodecamp.org/news/common-accessibility-errors-and-how-to-fix-them/",
   },
   {
