2378eec8975bfcc4debe410782b299ef28c04e41
Hannah Lin <ht.techx@gmail.com>
2023-10-16 17:17:36 -0400

    add a javascript question quizing users's knowledge on async await

--- src/data/javascript-quiz.ts
+++ src/data/javascript-quiz.ts
@@ -1968,6 +1968,18 @@ const javascriptQuiz = [
     Explanation:
       "An IIFE or Immediately Invoked Function Expression, is used to declare a private variable and to avoid polluting the global environment. If a function is declared with an IIFE by using a parenthesis around it, the variable declared inside it becomes private and its value can be accessed using a getter function inside the original function.",
     Link: "https://developer.mozilla.org/en-US/docs/Glossary/IIFE"
+  },
+  {
+    Question:
+      "In JavaScript, which of the following statements is correct regarding async await?",
+    Answer: "`async` automatically returns a promise",
+    Distractor1: "`await` keeps executing code regardless of promies status",
+    Distractor2: "`async` can be used independently without an async function",
+    Distractor3:
+      "Multiple `await` keywords can be used in a single line inside an `async` function",
+    Explanation:
+      "An `async function` declaration creates an [`AsyncFunction`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/AsyncFunction) object. Each time when an async function is called, it returns a new [`Promise`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise) which will be resolved with the value returned by the async function, or rejected with an exception uncaught within the async function.",
+    Link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function"
   }
 ];
 
