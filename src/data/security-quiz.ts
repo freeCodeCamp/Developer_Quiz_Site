be1b6e0f28b2c16027cebe6f15dd9b4a4008df05
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2023-10-18 00:21:19 -0400

    added security question

--- src/data/security-quiz.ts
+++ src/data/security-quiz.ts
@@ -561,6 +561,16 @@ const securityQuiz = [
     Explanation:
       "In the C programming language, the  gets() function is known to be vulnerable to buffer overflow attacks since it doesn't check if the input is larger than the buffer size.",
     Link: "https://www.freecodecamp.org/news/buffer-overflow-attacks/"
+  },
+  {
+    Question: "Which of the following operating systems are known as a hacking tool?",
+    Answer: "Kali Linux",
+    Distractor1: "Ubuntu",
+    Distractor2: "Windows",
+    Distractor3: "MacOS",
+    Explanation:
+      "Kali Linux is a Debian-based Linux distribution aimed at advanced Penetration Testing and Security Auditing.",
+    Link: "https://www.freecodecamp.org/news/how-to-install-kali-linux/"
   }
 ];
 
