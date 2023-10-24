a80869c3992b63fc8c326e824860cb22ea467deb
Jasmine <95772825+Jasmine-maryj@users.noreply.github.com>
2023-10-24 22:02:11 +0530

    chore: adding a question on cloud-native applications

--- src/data/cloud-computing-quiz.ts
+++ src/data/cloud-computing-quiz.ts
@@ -1,6 +1,17 @@
 const cloudComputingQuiz = [
   {
     Question:
+      "What is one of the key benefits of following CNCF standards in cloud-native app development?",
+    Answer: "Ensuring apps are built in the right way and reducing vendor lock-in",
+    Distractor1: "Increased vendor lock-in",
+    Distractor2: "Slower development process",
+    Distractor3: "Reduced compatibility with other services",
+    Explanation:
+      "Following CNCF standards ensures that cloud-native apps are built using best practices, reducing the risk of vendor lock-in and promoting compatibility with a wide range of cloud services and tools.",
+    Link: "https://www.freecodecamp.org/news/get-started-with-cloud-native/"
+  },
+  {
+    Question:
       "Which organization champions standards for cloud-native services and helps reduce vendor lock-in?",
     Answer: "CNCF (Cloud Native Computing Foundation)",
     Distractor1: "AWS (Amazon Web Services)",
