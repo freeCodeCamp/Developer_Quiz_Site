9631df4a3f9668f2ab7e895bf11c092712162c0d
moshe vilner <shootermv@gmail.com>
2023-12-22 08:13:57 +0200

    fix: restore deploy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Developer_Quiz_Site",
   plugins: [react()],
   root: "src",
   server: {
