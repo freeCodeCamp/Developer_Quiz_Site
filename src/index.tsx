adefec3a220eae8a46d9471268489038807603f2
moshe vilner <shootermv@gmail.com>
2024-01-05 09:22:03 +0200

    chore: upgrade react to 18 version

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 reportWebVitals();
