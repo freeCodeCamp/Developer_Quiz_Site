366b60922d53c54304eedb540d10e14f514cce8f
Moshe <shootermv@gmail.com>
2024-01-10 22:06:12 +0200

    fix: App and WelcomePage tests (#935)
    * test: make the App & Welcome tests pass
    
    * fix: add disable-next-line eslint statement

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "../App";
 import { cleanup } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 afterEach(cleanup);
 
 describe("App", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const root = createRoot(div!);
+    root.render(
+      <Router>
+        <App />
+      </Router>
+    );
   });
 });
--- src/__tests__/WelcomePage.test.tsx
+++ src/__tests__/WelcomePage.test.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import WelcomePage from "../components/WelcomePage";
 import { cleanup } from "@testing-library/react";
-
+import { BrowserRouter as Router } from "react-router-dom";
 afterEach(cleanup);
 
 describe("Welcome Page", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<WelcomePage start={undefined} />, div);
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const root = createRoot(div!);
+    root.render(
+      <Router>
+        <WelcomePage />
+      </Router>
+    );
   });
 });
