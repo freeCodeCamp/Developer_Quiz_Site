7a72eaee4ae05747b980c0bb86ef286b4a54324b
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-08-13 02:40:11 +0700

    chore: remove commented out code

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig, devices } from "@playwright/test";
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
