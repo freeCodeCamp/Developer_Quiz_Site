2e6137ea0147918c0d3aac2d61f3edc5a13ff68c
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-08-13 01:40:02 +0700

    chore: refactor can-finish-quiz.spec.ts

--- e2e/can-finish-quiz.spec.ts
+++ e2e/can-finish-quiz.spec.ts
@@ -8,35 +8,30 @@ test.beforeEach(async ({ page }) => {
   await page.getByRole("button", { name: "10", exact: true }).click();
 });
 
-test("after answer all the questions user should see 'results' page", async ({
+test("should show the results after the user has answered answer all questions", async ({
   page
 }) => {
   // loop through all the questions.
   for (let i = 1; i <= 10; i++) {
-    // Url should contain number of question
-    await expect(page.url()).toContain(`${i}/of/10`);
+    expect(page.url()).toContain(`${i}/of/10`);
 
     // Select the first option (no matter if it right or worng)
-    await page.getByRole("button").nth(0).click();
+    await page.getByRole("button").first().click();
 
-    // Click the submit button
-    await page.getByRole("button").nth(4).click();
+    await page.getByRole("button", { name: "Submit", exact: true }).click();
 
-    await expect(page.getByRole("dialog")).toBeVisible();
-    // Find the modal-dialog element
-    const modalDialog = await page.$(".modal-dialog");
+    const dialog = page.getByRole("dialog");
 
-    // Ensure the modal-dialog element exists
-    expect(modalDialog).not.toBeNull();
-
-    // Find the button inside the modal-dialog element
-    const button = await modalDialog.$("button");
-
-    // Click 'Next Question' button
-    await button.click();
-
-    if (i === 10) {
-      expect(page.url()).toContain("results");
-    }
+    await expect(dialog).toBeVisible();
+    await expect(dialog.getByText("Points:")).toBeVisible();
+    await expect(dialog.getByText("Your answer:")).toBeVisible();
+    await expect(dialog.getByText("Answer:", { exact: true })).toBeVisible();
+    await dialog.getByRole("button", { name: "Next Question" }).click();
   }
+
+  // After the 10th question, the results page shows up
+  await page.waitForURL("/#/quizzes/HTML/results");
+
+  await expect(page.getByRole("heading", { name: "Results" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Play again?" })).toBeVisible();
 });
