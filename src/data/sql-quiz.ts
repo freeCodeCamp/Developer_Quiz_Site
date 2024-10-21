699ed0ae51e550eb55fc5b89f3067a3dc337f9ff
Hetu <Hetu@hetukariya705.com>
2024-10-21 23:10:38 +0530

    SQL Question Added

--- src/data/sql-quiz.ts
+++ src/data/sql-quiz.ts
@@ -1,5 +1,15 @@
 const sqlQuiz = [
   {
+    Question: "In terms of Big-O complexity, what is the time complexity for lookups in a binary tree index?", 
+    Answer: "O(log n)",
+    Distractor1: "O(n)",
+    Distractor2: "O(n log n)",
+    Distractor3: "O(1)",
+    Explanation: 
+      "This is because a binary tree index allows for efficient searching by halving the search space at each step.",
+    Link: "https://www.freecodecamp.org/news/a-beginners-guide-to-sql/#heading-chapter-11-database-performance"
+  },
+  {
     Question: "Which of the following is the most effective way to protect against SQL injection attacks?", 
     Answer: "Using a modern SQL library that sanitizes SQL inputs",
     Distractor1: "Using strong passwords for database access",
