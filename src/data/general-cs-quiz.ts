4f6c1a0e906bbf87a1b63985cb367f31fbe48603
HetuKariya <131229641+HetuKariya@users.noreply.github.com>
2024-10-30 22:54:54 +0530

    Added SQL Question (#1181)
    * Added SQL Quiz Question
    
    * Solved the Changes
    
    * Added General-CS-Quiz-Question
    
    * Added Linux Question
    
    * Solved Issue
    
    * Final Review
    
    * SQL Question Added
    
    * Update sql-quiz.ts
    
    * Update linux-quiz.ts
    
    * Update general-cs-quiz.ts
    
    * Update sql-quiz.ts
    
    * fix: add in ford fulkerson algo
    
    * fix: move question to cs quiz
    
    ---------
    
    Co-authored-by: Hetu <Hetu@hetukariya705.com>
    Co-authored-by: Jessica Wilkins <67210629+jdwilkin4@users.noreply.github.com>

--- src/data/general-cs-quiz.ts
+++ src/data/general-cs-quiz.ts
@@ -1,5 +1,15 @@
 const generalCSQuiz = [
   {
+    Question: "In terms of Big-O complexity, what is the time complexity for lookups in a binary tree index?", 
+    Answer: "O(log n)",
+    Distractor1: "O(n)",
+    Distractor2: "O(n log n)",
+    Distractor3: "O(1)",
+    Explanation: 
+      "This is because a binary tree index allows for efficient searching by halving the search space at each step.",
+    Link: "https://www.freecodecamp.org/news/a-beginners-guide-to-sql/#heading-chapter-11-database-performance"
+  },
+  {
     Question: "What is the time complexity of the Ford-Fulkerson algorithm?",
     Answer: "O(V * E ^ 2)",
     Distractor1: "O(V + E)",
