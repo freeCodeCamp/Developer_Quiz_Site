5a0eb3ba795de76dba8e09a1ce7db75b90513a82
moshe vilner <shootermv@gmail.com>
2024-11-02 20:44:11 +0200

    chore: remove react-bootstrap package

--- src/components/QuizModal.tsx
+++ src/components/QuizModal.tsx
@@ -1,5 +1,5 @@
-import Modal from "react-bootstrap/Modal";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Modal from "./Modal";
+
 import React, { MouseEventHandler } from "react";
 
 interface QuizQuestion {
@@ -18,56 +18,54 @@ const QuizModal: React.FC<QuizQuestion> = QuizQuestion => {
   const sadFaceUnicode = "\uD83D\uDE14";
   const answerIcon = QuizQuestion.correct ? lightBulbUnicode : sadFaceUnicode;
   return (
-    <>
-      <Modal className="modal-styles" show={QuizQuestion.show}>
-        <Modal.Header>
-          <Modal.Title>
-            <h2 className="modal-text">
-              {answerIcon} {QuizQuestion.message}
-            </h2>
-            <h3 className="modal-text" data-testid="modal-points">
-              Points: {QuizQuestion.points}
-            </h3>
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>
-            <b>Your Answer:</b>
-          </p>
-          <p>{QuizQuestion.chosenAnswer}</p>
-          <p>
-            <b>Answer:</b>
-          </p>
-          <p>{QuizQuestion.displayExplanation}</p>
+    <Modal show={QuizQuestion.show}>
+      <Modal.Header>
+        <Modal.Title>
+          <h2 className="modal-text">
+            {answerIcon} {QuizQuestion.message}
+          </h2>
+          <h3 className="modal-text" data-testid="modal-points">
+            Points: {QuizQuestion.points}
+          </h3>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          <b>Your Answer:</b>
+        </p>
+        <p>{QuizQuestion.chosenAnswer}</p>
+        <p>
+          <b>Answer:</b>
+        </p>
+        <p>{QuizQuestion.displayExplanation}</p>
+        <a
+          className="modal-link"
+          href={QuizQuestion.showReference}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn more with this helpful resource
+        </a>
+        <br />
+        <br />
+        <p>
+          Wanna learn how to code? <br /> Download the free{" "}
           <a
             className="modal-link"
-            href={QuizQuestion.showReference}
             target="_blank"
             rel="noopener noreferrer"
+            href="https://www.freecodecamp.org/news/learn-to-code-rpg/"
           >
-            Learn more with this helpful resource
+            Learn to Code RPG game
           </a>
-          <br />
-          <br />
-          <p>
-            Wanna learn how to code? <br /> Download the free{" "}
-            <a
-              className="modal-link"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.freecodecamp.org/news/learn-to-code-rpg/"
-            >
-              Learn to Code RPG game
-            </a>
-          </p>
-        </Modal.Body>
-        <Modal.Footer>
-          <button className="modal-btn" onClick={QuizQuestion.nextQuestion}>
-            Next Question
-          </button>
-        </Modal.Footer>
-      </Modal>
-    </>
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <button className="modal-btn" onClick={QuizQuestion.nextQuestion}>
+          Next Question
+        </button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 export default QuizModal;
